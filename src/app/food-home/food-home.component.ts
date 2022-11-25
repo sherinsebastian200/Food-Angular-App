9b729d2da286ac738db1720b8b1e2be6d023c81d
sherin200 <sherinsebastian21>
2022-11-25 07:25:00 +0530

    home

---
+++ src/app/food-home/food-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-home',
+  templateUrl: './food-home.component.html',
+  styleUrls: ['./food-home.component.css']
+})
+export class FoodHomeComponent {
+
+}
