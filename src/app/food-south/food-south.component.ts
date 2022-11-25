a10a02d6740d928578c7e09aa42a562c0fea635d
sherin200 <sherinsebastian21>
2022-11-25 08:00:44 +0530

    update food items

---
+++ src/app/food-south/food-south.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-south',
+  templateUrl: './food-south.component.html',
+  styleUrls: ['./food-south.component.css']
+})
+export class FoodSouthComponent {
+
+}
